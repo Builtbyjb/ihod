42fd4d2e29d16694566416856d41d26c9acce623
Builtbyjb <awotideajibola@gmail.com>
2026-04-29 11:11:33 -0400

    minor fix

--- apps/server/drizzle/migrations/0003_sparkling_zarda.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `invoices` ADD `items` text DEFAULT '[]';--> statement-breakpoint
-ALTER TABLE `invoices` DROP COLUMN `amount`;--> statement-breakpoint
-ALTER TABLE `invoices` DROP COLUMN `currency`;
