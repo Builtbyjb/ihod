2851257961cb568c8dea3ff96f74d8e3b73bbf71
Builtbyjb <awotideajibola@gmail.com>
2026-05-27 23:46:00 -0400

    minor fix

--- apps/web/src/components/InvoiceTemplates/DefaultTemplate.tsx
+++ apps/web/src/components/InvoiceTemplates/DefaultTemplate.tsx
@@ -14,7 +14,7 @@ type InvoicePDFProps = {
 };
 
 export const DefaultInvoiceTemplate = forwardRef<HTMLDivElement, InvoicePDFProps>(
-  ({ invoice, client, bussinessname, logoURL }, ref) => (
+  ({ invoice, client, bussinessname }, ref) => (
     <main ref={ref} className="bg-white w-[210mm] min-h-[290mm] mx-auto p-[10mm] text-gray-900 print:p-[10mm]">
       <div className="h-full flex flex-col justify-between">
         {/* Top Content */}
